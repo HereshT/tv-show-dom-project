5c64eb7f79d321684aee37c387c6720f08d568eb
HereshT <hiresh.turky@gmail.com>
2023-04-26 16:10:37 +0100

    Level 200 Add Search Done.

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-//You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
+  const searchInput = document.createElement("input");
+  searchInput.id = "search-input";
+  searchInput.type = "text";
+  searchInput.placeholder = "Search episodes...";
+  const searchResults = document.createElement("p");
+  searchResults.id = "search-results";
+
+  const rootElem = document.getElementById("root");
+  rootElem.appendChild(searchInput);
+  rootElem.appendChild(searchResults);
+
+  searchInput.addEventListener("input", (event) => {
+    searchEpisodes(event.target.value, allEpisodes);
+  });
+
   makePageForEpisodes(allEpisodes);
 }
 
@@ -8,7 +22,7 @@ function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   displayEpisodes(episodeList, rootElem);
 }
-function displayEpisodes(episodes, rootElem) {
+function displayEpisodes(episodes, rootElem, searchTerm = "") {
   const container = document.createElement("div");
   container.className = "episodes-container";
 
@@ -17,10 +31,12 @@ function displayEpisodes(episodes, rootElem) {
     episodeCard.className = "episode-card";
 
     const title = document.createElement("h2");
-    title.textContent = `${episode.name} - S${String(episode.season).padStart(
-      2,
-      "0"
-    )}E${String(episode.number).padStart(2, "0")}`;
+    title.innerHTML = highlightSearchTerm(
+      `${episode.name} - S${String(episode.season).padStart(2, "0")}E${String(
+        episode.number
+      ).padStart(2, "0")}`,
+      searchTerm
+    );
     episodeCard.appendChild(title);
 
     const image = document.createElement("img");
@@ -28,13 +44,49 @@ function displayEpisodes(episodes, rootElem) {
     episodeCard.appendChild(image);
 
     const summary = document.createElement("p");
-    summary.innerHTML = episode.summary;
+    summary.innerHTML = highlightSearchTerm(episode.summary, searchTerm);
     episodeCard.appendChild(summary);
 
     container.appendChild(episodeCard);
   });
 
   rootElem.appendChild(container);
+}
+
+function highlightSearchTerm(text, searchTerm) {
+  if (!searchTerm) return text;
+
+  const searchTermLower = searchTerm.toLowerCase();
+  const textLower = text.toLowerCase();
+  const regex = new RegExp("(" + escapeRegExp(searchTermLower) + ")", "gi");
+
+  return text.replace(regex, (match) => {
+    return "<mark>" + match + "</mark>";
+  });
+}
+
+function escapeRegExp(string) {
+  return string.replace(/[.*+\-?^${}()|[\]\\]/g, "\\$&");
+}
+
+function searchEpisodes(searchTerm, allEpisodes) {
+  const rootElem = document.getElementById("root");
+  const searchResults = document.getElementById("search-results");
+
+  const filteredEpisodes = allEpisodes.filter((episode) => {
+    const searchTermLowerCase = searchTerm.toLowerCase();
+    return (
+      episode.name.toLowerCase().includes(searchTermLowerCase) ||
+      episode.summary.toLowerCase().includes(searchTermLowerCase)
+    );
+  });
+
+  searchResults.textContent = `Displaying ${filteredEpisodes.length} episode(s)`;
+  const oldContainer = document.querySelector(".episodes-container");
+  const newContainer = document.createElement("div");
+  newContainer.className = "episodes-container";
+  rootElem.replaceChild(newContainer, oldContainer);
+  displayEpisodes(filteredEpisodes, newContainer);
 }
 
 window.onload = setup;
