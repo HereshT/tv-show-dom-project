1a09f8f3703c96032a6467d13363f6d6d7a7ad2a
HereshT <hiresh.turky@gmail.com>
2023-04-26 11:48:33 +0100

    Level 100 Minimal features Done.

--- script.js
+++ script.js
@@ -6,7 +6,35 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  displayEpisodes(episodeList, rootElem);
+}
+function displayEpisodes(episodes, rootElem) {
+  const container = document.createElement("div");
+  container.className = "episodes-container";
+
+  episodes.forEach((episode) => {
+    const episodeCard = document.createElement("div");
+    episodeCard.className = "episode-card";
+
+    const title = document.createElement("h2");
+    title.textContent = `${episode.name} - S${String(episode.season).padStart(
+      2,
+      "0"
+    )}E${String(episode.number).padStart(2, "0")}`;
+    episodeCard.appendChild(title);
+
+    const image = document.createElement("img");
+    image.src = episode.image.medium;
+    episodeCard.appendChild(image);
+
+    const summary = document.createElement("p");
+    summary.innerHTML = episode.summary;
+    episodeCard.appendChild(summary);
+
+    container.appendChild(episodeCard);
+  });
+
+  rootElem.appendChild(container);
 }
 
 window.onload = setup;
